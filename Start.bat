1afa2f5581c3fbef8b6a06dee17c86c3209fbbff
MorikTV <110910764+MorikTV@users.noreply.github.com>
2024-09-22 18:09:57 -0700

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,14 +1,20 @@
 @echo off
+
 IF NOT EXIST "venv\Scripts\activate.bat" (
+    echo Creating virtual environment...
     python -m venv venv
+    echo Activating virtual environment...
+    call venv\Scripts\activate
+    echo Installing dependencies from requirements.txt...
+    pip install -r src/requirements.txt
+) ELSE (
+    echo Activating existing virtual environment...
+    call venv\Scripts\activate
 )
-
-call venv\Scripts\activate
-
-pip install -r src/requirements.txt
 
 cls
 
+echo Starting the application...
 python src/main.py
 
 cmd /k
